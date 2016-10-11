ffea68f19be458d0bc03109e80f51b13a4e928c8
Wijit Siripun <wijit.siripun@gmail.com>
2016-10-11 16:18:07 -0500

    adding two servlet and those eclipse-related files

---
+++ server/reservation/src/java/com/example/servlet/HelloWorldServlet.java
@@ -0,0 +1,21 @@
+package com.example.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class HelloWorldServlet extends HttpServlet
+{
+	@Override
+	public void service( HttpServletRequest request, HttpServletResponse response )
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		out.println( "<html><body>Hello World WIJS 1111 HELLO WORLD</body></html>" );
+//		response.sendRedirect("http://www.cnn.com"); 
+		out.close();
+	}
+}
