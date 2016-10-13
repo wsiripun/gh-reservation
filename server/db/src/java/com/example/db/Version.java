e225cae2610026c610304c74d4ff1b0ad3d4b4a4
Wijit Siripun <wijit.siripun@gmail.com>
2016-10-13 16:31:29 -0500

    Testing MySQL connection by retrieving its version

---
+++ server/db/src/java/com/example/db/Version.java
@@ -0,0 +1,62 @@
+package com.example.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Version {
+
+    public static void main(String[] args) {
+
+        Connection con = null;
+        Statement st = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/wijs_reservation";
+        String user = "dbuser";
+        String password = "dbpassword";
+
+        try {
+            
+            con = DriverManager.getConnection(url, user, password);
+            st = con.createStatement();
+            rs = st.executeQuery("SELECT VERSION()");
+
+            if (rs.next()) {
+                
+                System.out.println("WIJS: MySQL Version is:  " + rs.getString(1));
+            }
+
+        } catch (SQLException ex) {
+        
+            Logger lgr = Logger.getLogger(Version.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+        } finally {
+            
+            try {
+                
+                if (rs != null) {
+                    rs.close();
+                }
+                
+                if (st != null) {
+                    st.close();
+                }
+                
+                if (con != null) {
+                    con.close();
+                }
+
+            } catch (SQLException ex) {
+                
+                Logger lgr = Logger.getLogger(Version.class.getName());
+                lgr.log(Level.WARNING, ex.getMessage(), ex);
+            }
+        }
+    }
+}
